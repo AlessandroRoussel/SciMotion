1242a70113b7fe4c4630a068938983a6ceecac69
Alessandro Roussel <29360713+AlessandroRoussel@users.noreply.github.com>
2024-10-24 22:26:07 +0200

    Update AnimationService

--- editing/services/animation_service.py
+++ editing/services/animation_service.py
@@ -6,7 +6,11 @@ package, concerning animation capabilities. This includes
 keyframing, interpolating between keyframes...
 """
 
+
 from utils.singleton import Singleton
+from datatypes.datatype import DataType
+from editing.entities.parameter import Parameter
+from editing.entities.keyframe import Keyframe
 
 
 class AnimationService(metaclass=Singleton):
@@ -14,3 +18,15 @@ class AnimationService(metaclass=Singleton):
 
     def __init__(self):
         pass
+
+    def add_keyframe(parameter: Parameter, keyframe: Keyframe):
+        """Add a keyframe to a parameter."""
+        pass
+
+    def remove_keyframe(parameter: Parameter, keyframe: Keyframe):
+        """Remove a keyframe from a parameter."""
+        pass
+
+    def get_value_from_frame(parameter: Parameter, frame: int) -> DataType:
+        """Interpolate the value of a parameter at a given frame."""
+        return None
