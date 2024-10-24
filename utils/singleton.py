bac207970305fa9b579da508587ab30595524b68
Alessandro Roussel <29360713+AlessandroRoussel@users.noreply.github.com>
2024-10-24 17:21:38 +0200

    Add Singleton metaclass

---
+++ utils/singleton.py
@@ -0,0 +1,21 @@
+"""
+A singleton metaclass.
+
+A singleton class ensures that only one
+instance of the class can ever exist.
+"""
+
+
+class Singleton:
+    """A singleton metaclass."""
+
+    _instances = dict()
+
+    def __call__(cls, *args, **kwargs):
+        """Create instance if needed only."""
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        else:
+            instance = cls._instances[cls]
+        return instance
