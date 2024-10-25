70c87e0858af614a3b75bdc014d398b35a92385b
Alessandro Roussel <29360713+AlessandroRoussel@users.noreply.github.com>
2024-10-26 01:22:33 +0200

    Update EffectService

--- editing/services/effect_service.py
+++ editing/services/effect_service.py
@@ -9,11 +9,14 @@ adding effects to layers...
 
 import json
 from pathlib import Path
+from typing import Dict
 
 from datatypes import DataTypeName
 from utils.singleton import Singleton
 from editing.entities.effect import Effect
+from editing.entities.effect_instance import EffectInstance
 from editing.entities.effect_repository import EffectRepository
+from editing.entities.parameter import Parameter
 from editing.entities.parameter_template import ParameterTemplate
 from rendering.entities.shader_program import ShaderProgram
 
@@ -43,7 +46,10 @@ class EffectService(metaclass=Singleton):
                                                            _json_data)
                     _repository[_unique_name] = _effect
 
-    def create_effect_from_data(self, glsl_code, json_data):
+    def create_effect_from_data(self,
+                                glsl_code: str,
+                                json_data: Dict
+                                ) -> Effect:
         """Load an effect given its glsl and json data."""
         _shader_program = ShaderProgram(glsl_code)
         _title = ""
@@ -105,3 +111,28 @@ class EffectService(metaclass=Singleton):
                       _title,
                       _flags,
                       _parameter_template_list)
+
+    def instanciate_effect(self, effect_unique_name: str) -> EffectInstance:
+        """Create an EffectInstance based on an Effect in the repository."""
+        _repository = EffectRepository().get_repository()
+        if effect_unique_name not in _repository:
+            raise ValueError(f"Couldn't find {effect_unique_name}"
+                             f"in the effects repository.")
+        _effect = _repository[effect_unique_name]
+        _parameter_list = []
+        for _parameter_template in _effect.get_parameter_template_list():
+            _title = _parameter_template.get_title()
+            _accepts_keyframes = _parameter_template.get_accepts_keyframes()
+            _data_type = _parameter_template.get_data_type()
+            _default_value = _parameter_template.get_default_value()
+            _min_value = _parameter_template.get_min_value()
+            _max_value = _parameter_template.get_max_value()
+            _parameter = Parameter(title=_title,
+                                   accepts_keyframes=_accepts_keyframes,
+                                   data_type=_data_type,
+                                   default_value=_default_value,
+                                   min_value=_min_value,
+                                   max_value=_max_value)
+            _parameter_list.append(_parameter)
+        _effect_instance = EffectInstance(effect_unique_name, _parameter_list)
+        return _effect_instance
